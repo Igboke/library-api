3012271592cbd667826103bcdac96bd12e55aa9a
Igboke Daniel <danieligboke669@gmail.com>
2025-05-18 18:53:48 +0100

    Initial commit

---
+++ src/test/java/com/igboke/library_api/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igboke.library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
