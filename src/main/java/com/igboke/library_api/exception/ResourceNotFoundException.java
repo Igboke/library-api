46fe63597bdf995418e6fa3e757979072fab318f
Igboke Daniel <danieligboke669@gmail.com>
2025-05-18 20:53:53 +0100

    Add ResourceNotFoundException class for handling resource not found errors

---
+++ src/main/java/com/igboke/library_api/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.igboke.library_api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+
+        super(message);
+
+    }
+
+}
+
