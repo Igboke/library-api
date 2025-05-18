b27524afcfaf0668c4f6146b199863bf9c5063be
Igboke Daniel <danieligboke669@gmail.com>
2025-05-18 20:38:03 +0100

    Add Book entity model with attributes and constructors

---
+++ src/main/java/com/igboke/library_api/model/Book.java
@@ -0,0 +1,41 @@
+package com.igboke.library_api.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Column(name = "isbn", unique = true)
+    private String isbn;
+
+    @Column(name = "publication_year")
+    private int publishedYear;
+
+    @Column(name = "description",columnDefinition = "TEXT")
+    private String description;
+
+    // Constructor without ID for creating new books
+    public Book(String title, String author, String isbn, int publishedYear, String description) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publishedYear = publishedYear;
+        this.description = description;
+    }
+
+}
